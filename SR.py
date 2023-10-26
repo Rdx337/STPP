df4658105a06d72596e0631754aa5ae772316ad7
Sabbir Bhai 36 <138810861+Rdx337@users.noreply.github.com>
2023-10-26 22:26:16 +0600

    Update SR.py

--- SR.py
+++ SR.py
@@ -1,6 +1,4 @@
-
 #-----------------[ IMPORT-MODULE ]-------------------
-from bs4 import BeautifulSoup as sop
 import requests,bs4,json,os,sys,random,datetime,time,re
 import urllib3,rich,base64
 from rich.table import Table as me
